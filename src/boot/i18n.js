7fc9269d082f312bba73250a1ecd395afdba591d
Zhiquan <x.zhiquan@gmail.com>
2020-10-29 15:23:38 +0800

    move the i18n dict init into core module as we should load the transloates according to the module dependency order.

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,42 +1,24 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-import config from '../config';
 
-let messages = {
+const messages = {
   'en-us': {
     error404: 'Sorry, nothing here...',
     goback: 'Go Back',
+    LOGIN: 'Login',
+    LOGOUT: 'Logout',
+    REGISTER: 'Register',
+    SETTINGS: 'Settings',
   },
   'zh-cn': {
     error404: '抱歉，此页面不存在...',
     goback: '返回',
+    LOGIN: '登陆',
+    LOGOUT: '退出登陆',
+    REGISTER: '注册',
+    SETTINGS: '设置',
   },
 };
-
-// import translations from modules
-const context = require.context('../modules', true, /\/i18n\/[^/]+\/[^/]+\.js$/);
-const contextKeys = context.keys();
-for (let i = 0; i < contextKeys.length; i += 1) {
-  const key = contextKeys[i];
-
-  const pathList = key.split('/');
-  const i18nIndex = pathList.lastIndexOf('i18n');
-
-  if (i18nIndex >= 0 || i18nIndex < pathList.length - 2) {
-    const lang = pathList[i18nIndex + 1];
-    const langDict = context(key).default;
-
-    const langKeys = Object.keys(messages);
-    if (langKeys.indexOf(lang) >= 0) {
-      messages[lang] = Object.assign({}, messages[lang], langDict);
-    } else {
-      messages[lang] = langDict;
-    }
-  }
-}
-
-// load messages from the configuration
-messages = Object.merge(messages, config.i18n);
 
 Vue.use(VueI18n);
 
@@ -45,12 +27,17 @@ const DEFAULT_LANGUAGE = 'zh-cn';
 const i18n = new VueI18n({
   locale: DEFAULT_LANGUAGE,
   fallbackLocale: DEFAULT_LANGUAGE,
-  messages,
 });
 
 export default ({ app }) => {
   // Set i18n instance on app
   app.i18n = i18n;
+
+  if (app.store && app.store.i18nMessages) {
+    Object.keys(app.store.i18nMessages).forEach((ik) => {
+      i18n.setLocaleMessage(ik, { ...app.store.i18nMessages[ik], ...(messages[ik] || {}) });
+    });
+  }
 };
 
 export { i18n };
