1d84ce85171bd0b4051b39afa72091790cdee4f5
aslancer <x.zhiquan@gmail.com>
2021-05-06 18:04:31 +0800

    make get request cancelable

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -18,9 +18,25 @@ const axiosInstance = axios.create({
   timeout: 3000 * 1000,
 });
 
+axiosInstance.interceptors.request.use((cfg) => {
+  cfg.cancelToken = new axios.CancelToken((tk) => {
+    cfg.cancelTokenFunc = tk;
+  });
+  return cfg;
+});
+
 // respone interceptor
 axiosInstance.interceptors.response.use(
-  (response) => response.data,
+  (response) => {
+    if (response.config && response.config.shouldCancelRequest
+      && ((typeof response.config.shouldCancelRequest === 'function')
+        ? response.config.shouldCancelRequest() : true)) {
+      response.config.cancelTokenFunc();
+      throw new Error('request canceled');
+    } else {
+      return response.data;
+    }
+  },
   (error) => {
     if (error && error.response && error.response.status === 401) {
       if (window.location.pathname !== '/login') { window.location.pathname = '/login'; }
@@ -34,7 +50,7 @@ axiosInstance.interceptors.response.use(
 
 const mockIt = (url, method) => {
   const theMock = Mocks.find(
-    (mk) => mk.method === method && new RegExp(mk.url).test(url),
+    mk => mk.method === method && new RegExp(mk.url).test(url),
   );
   if (theMock && theMock.func) {
     return new Promise((resolve) => {
@@ -62,6 +78,10 @@ const getQueryKVPare = (o, parent = '') => {
 
 const getRequest = (url, options, newWin = false) => {
   let queryString = '';
+
+  const shouldCancel = { shouldCancelRequest: options && options.cancel_request };
+  if (options) delete options.cancel_request;
+
   if (options && Object.keys(options).length) {
     if (url.indexOf('?') > 0) {
       queryString += '&';
@@ -76,7 +96,7 @@ const getRequest = (url, options, newWin = false) => {
     return window.open(`${config.baseUrl}/${encodeURI(url)}${queryString}`);
   }
 
-  return mockIt(`${url}${queryString}`, 'get') || axiosInstance.get(encodeURI(url) + queryString);
+  return mockIt(`${url}${queryString}`, 'get') || axiosInstance.get(encodeURI(url) + queryString, shouldCancel);
 };
 
 const postRequest = (url, data) => {
